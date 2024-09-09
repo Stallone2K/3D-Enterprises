e7552cb90b98eaac0ecfadda9c68903fafce0201
StallonesHyper <stallonefernandess@gmail.com>
2024-09-09 19:59:45 +0530

    Stage 1 Meeting

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ import {
   Environment,
   OrbitControls,
 } from "@react-three/drei";
+// import Cards from "./components/Cards";
+// import NavBar from "./components/NavBar";
 function App() {
   return (
     <>
+      {/* <NavBar></NavBar> */}
       <Canvas
         className="font-poppins bg-slate-300"
-        camera={{ fov: 64, position: [2.3, 2.5, 2.3] }}
+        camera={{ fov: 72, position: [2.3, 2.5, 2.3] }}
       >
         <OrbitControls
           enableZoom={false}
@@ -28,7 +31,7 @@ function App() {
           shadow-bias={-0.0001}
         />
         <Environment preset="studio" />
-        <ScrollControls pages={3} damping={0.1}>
+        <ScrollControls pages={4} damping={0.1}>
           <Scroll>
             {/* Model HERE */}
             <Chair></Chair>
@@ -38,30 +41,30 @@ function App() {
             <div className="text-center">
               <div className=" flex justify-center items-center h-screen flex-col m-10">
                 <h1 className="text-9xl font-bold">
-                  Transform Your Space With Timeless Furniture
+                  Empowering Your Brand Growth.
                 </h1>
                 <p className="text-lg font-bold">
-                  Discover The Perfect Blend Of Comfort, Style, And
-                  Functionality.
+                  Providing High-Impact Services For Your Business
                 </p>
                 <button
                   type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
                 >
-                  <a href="https://dreamhomestore.co.uk/">Shop Now</a>
+                  <a href="https://dreamhomestore.co.uk/">Get In Touch</a>
                 </button>
               </div>
               {/* 2 */}
               <div className="flex justify-center items-center h-screen flex-col">
-                <h1 className="top-[100vh] text-6xl font-bold">
-                  Explore Our Collection
-                </h1>
+                <h1 className="top-[100vh] text-6xl font-bold">About Us</h1>
+                {/* <Cards></Cards> */}
               </div>
               {/* 3 */}
               <div className="h-screen flex justify-center items-center flex-col">
-                <h1 className="top-[200vh] text-9xl font-bold">
-                  Crafting Timeless Elegance
-                </h1>
+                <h1 className="top-[200vh] text-6xl font-bold">Why Us ?</h1>
+                {/* <Cards></Cards> */}
+              </div>
+              <div className="h-screen flex justify-center items-center flex-col">
+                <h1 className="top-[200vh] text-6xl font-bold">Services</h1>
                 {/* <Cards></Cards> */}
               </div>
             </div>
